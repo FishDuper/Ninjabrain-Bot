f45747c9cbea1949eb3eb90e54ebfb7d9f076dfd
FishDuper <gsgsgs03gs@gmail.com>
2026-07-13 01:29:37 -0500

    Add files via upload
    auto reset timer with customizable time

--- src/main/java/ninjabrainbot/io/AutoResetTimer.java
+++ src/main/java/ninjabrainbot/io/AutoResetTimer.java
@@ -4,47 +4,57 @@ import javax.swing.Timer;
 
 import ninjabrainbot.event.DisposeHandler;
 import ninjabrainbot.event.IDisposable;
+
 import ninjabrainbot.io.preferences.NinjabrainBotPreferences;
 import ninjabrainbot.model.actions.IActionExecutor;
 import ninjabrainbot.model.actions.common.ResetAction;
 import ninjabrainbot.model.datastate.IDataState;
 import ninjabrainbot.model.domainmodel.IDomainModel;
 
+
 public class AutoResetTimer extends Timer implements IDisposable {
 
-	private static final int AUTO_RESET_DELAY = 15 * 60 * 1000;
+    private final NinjabrainBotPreferences preferences;
+    final DisposeHandler disposeHandler = new DisposeHandler();
 
-	private final NinjabrainBotPreferences preferences;
+    private static int computeInitialDelay(NinjabrainBotPreferences preferences) {
+        return (int)(preferences.AutoResetDelay.get() * 60 * 1000);
+    }
 
-	final DisposeHandler disposeHandler = new DisposeHandler();
+    public AutoResetTimer(IDataState dataState, IDomainModel domainModel, IActionExecutor actionExecutor, NinjabrainBotPreferences preferences) {
+        super(computeInitialDelay(preferences), null);
+        this.preferences = preferences;
 
-	public AutoResetTimer(IDataState dataState, IDomainModel domainModel, IActionExecutor actionExecutor, NinjabrainBotPreferences preferences) {
-		super(AUTO_RESET_DELAY, null);
-		this.preferences = preferences;
-		addActionListener(p -> {
-			if (!dataState.locked().get())
-				actionExecutor.executeImmediately(new ResetAction(domainModel));
-			restart();
-			stop();
-		});
+        addActionListener(p -> {
+            if (!dataState.locked().get())
+                actionExecutor.executeImmediately(new ResetAction(domainModel));
+            restart();
+            stop();
+        });
 
-		refresh();
-		disposeHandler.add(domainModel.whenModified().subscribe(this::refresh));
-		disposeHandler.add(preferences.autoReset.whenModified().subscribe(this::refresh));
-	}
+        refresh();
+        disposeHandler.add(domainModel.whenModified().subscribe(this::refresh));
+        disposeHandler.add(preferences.autoReset.whenModified().subscribe(this::refresh));
+        disposeHandler.add(preferences.AutoResetDelay.whenModified().subscribe(this::refresh));
+    }
 
-	private void refresh() {
-		restart();
-		if (preferences.autoReset.get()) {
-			start();
-		} else {
-			stop();
-		}
-	}
+    private void refresh() {
+        int delay = (int)(preferences.AutoResetDelay.get() * 60 * 1000);
 
-	@Override
-	public void dispose() {
-		disposeHandler.dispose();
-	}
+        setDelay(delay);
+        setInitialDelay(delay);
 
+        restart();
+
+        if (preferences.autoReset.get()) {
+            start();
+        } else {
+            stop();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        disposeHandler.dispose();
+    }
 }
