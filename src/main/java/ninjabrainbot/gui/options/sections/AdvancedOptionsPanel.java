cd3db46077e98af16d175dc354848ede50120a67
FishDuper <gsgsgs03gs@gmail.com>
2026-07-13 01:24:50 -0500

    Add files via upload
    add GUI options for custom timer and auto-lock features

--- src/main/java/ninjabrainbot/gui/options/sections/AdvancedOptionsPanel.java
+++ src/main/java/ninjabrainbot/gui/options/sections/AdvancedOptionsPanel.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
 
 import ninjabrainbot.event.DisposeHandler;
 import ninjabrainbot.gui.buttons.FlatButton;
@@ -28,89 +29,96 @@ import ninjabrainbot.util.I18n;
 
 public class AdvancedOptionsPanel extends JPanel {
 
-	private final StyleManager styleManager;
-	private final NinjabrainBotPreferences preferences;
-	private final ICalibratorFactory calibratorFactory;
-	private final IActionExecutor actionExecutor;
-	private final JFrame owner;
+    private final StyleManager styleManager;
+    private final NinjabrainBotPreferences preferences;
+    private final ICalibratorFactory calibratorFactory;
+    private final IActionExecutor actionExecutor;
+    private final JFrame owner;
 
-	private final FloatPreferencePanel sigmaAlt;
-	private HotkeyPanel sigmaAltHotkey;
+    private final FloatPreferencePanel sigmaAlt;
+    private HotkeyPanel sigmaAltHotkey;
 
-	public AdvancedOptionsPanel(StyleManager styleManager, NinjabrainBotPreferences preferences, ICalibratorFactory calibratorFactory, IActionExecutor actionExecutor, JFrame owner, DisposeHandler disposeHandler) {
-		this.styleManager = styleManager;
-		this.preferences = preferences;
-		this.calibratorFactory = calibratorFactory;
-		this.actionExecutor = actionExecutor;
-		this.owner = owner;
-		setOpaque(false);
-		setLayout(new GridLayout(1, 2, 2 * OptionsFrame.PADDING, 0));
-		setBorder(new EmptyBorder(2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING));
-		JPanel column1 = new StackPanel(2 * OptionsFrame.PADDING);
-		JPanel column2 = new StackPanel(3 * OptionsFrame.PADDING);
-		column1.setOpaque(false);
-		column2.setOpaque(false);
-		add(column1);
-		add(column2);
+    public AdvancedOptionsPanel(StyleManager styleManager, NinjabrainBotPreferences preferences, ICalibratorFactory calibratorFactory, IActionExecutor actionExecutor, JFrame owner, DisposeHandler disposeHandler) {
+        this.styleManager = styleManager;
+        this.preferences = preferences;
+        this.calibratorFactory = calibratorFactory;
+        this.actionExecutor = actionExecutor;
+        this.owner = owner;
 
-		FloatPreferencePanel sigmaPanel = new FloatPreferencePanel(styleManager, I18n.get("settings.standard_deviation"), preferences.sigma);
-		column1.add(sigmaPanel);
-		JButton calibrateButton = new FlatButton(styleManager, I18n.get("settings.calibrate_standard_deviation")) {
-			@Override
-			public int getTextSize(SizePreference p) {
-				return p.TEXT_SIZE_SMALL;
-			}
-		};
-		calibrateButton.addActionListener(p -> startCalibrating(false));
-		calibrateButton.setAlignmentX(0.5f);
-		column1.add(calibrateButton);
+        setOpaque(false);
+        setLayout(new GridLayout(1, 2, 2 * OptionsFrame.PADDING, 0));
+        setBorder(new EmptyBorder(2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING));
 
-		FloatPreferencePanel sigmaManualPanel = new FloatPreferencePanel(styleManager, I18n.get("settings.standard_deviation_manual"), preferences.sigmaManual);
-		column1.add(sigmaManualPanel);
-		JButton manualCalibrateButton = new FlatButton(styleManager, I18n.get("settings.calibrate_standard_deviation_manual")) {
-			@Override
-			public int getTextSize(SizePreference p) {
-				return p.TEXT_SIZE_SMALL;
-			}
-		};
-		manualCalibrateButton.addActionListener(p -> startCalibrating(true));
-		manualCalibrateButton.setAlignmentX(0.5f);
-		column1.add(manualCalibrateButton);
+        JPanel column1 = new StackPanel(2 * OptionsFrame.PADDING);
+        JPanel column2 = new StackPanel(3 * OptionsFrame.PADDING);
+        column1.setOpaque(false);
+        column2.setOpaque(false);
+        add(column1);
+        add(column2);
 
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.enable_standard_deviation_toggle"), preferences.useAltStd));
-		sigmaAlt = new FloatPreferencePanel(styleManager, I18n.get("settings.alt_standard_deviation"), preferences.sigmaAlt);
-		sigmaAlt.setEnabled(preferences.useAltStd.get());
-		column1.add(sigmaAlt);
-		if (KeyboardListener.registered) {
-			sigmaAltHotkey = new HotkeyPanel(styleManager, I18n.get("settings.alt_std_on_last_angle"), preferences.hotkeyAltStd);
-			sigmaAltHotkey.setEnabled(preferences.useAltStd.get());
-			column1.add(sigmaAltHotkey);
-		}
+        FloatPreferencePanel sigmaPanel = new FloatPreferencePanel(styleManager, I18n.get("settings.standard_deviation"), preferences.sigma);
+        column1.add(sigmaPanel);
 
-		column2.add(new DoublePreferencePanel(styleManager, I18n.get("settings.crosshair_correction"), preferences.crosshairCorrection));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.show_angle_errors"), preferences.showAngleErrors));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.color_negative_coords"), preferences.colorCodeNegativeCoords));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.use_advanced_stronghold_statistics"), preferences.useAdvStatistics));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.use_alternative_clipboard_reader"), preferences.altClipboardReader));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.enable_api"), preferences.enableHttpServer)
-				.withWikiButton(new WikiButton(styleManager, "https://github.com/Ninjabrain1/Ninjabrain-Bot/wiki/API")));
-		column2.add(new CheckboxPanel(styleManager, I18n.get("settings.save_state"), preferences.saveState));
+        JButton calibrateButton = new FlatButton(styleManager, I18n.get("settings.calibrate_standard_deviation")) {
+            @Override
+            public int getTextSize(SizePreference p) {
+                return p.TEXT_SIZE_SMALL;
+            }
+        };
+        calibrateButton.addActionListener(p -> startCalibrating(false));
+        calibrateButton.setAlignmentX(0.5f);
+        column1.add(calibrateButton);
 
-		disposeHandler.add(preferences.useAltStd.whenModified().subscribeEDT(this::setAltSigmaEnabled));
-		disposeHandler.add(preferences.sigma.whenModified().subscribeEDT(sigmaPanel::updateValue));
-		disposeHandler.add(preferences.sigmaManual.whenModified().subscribeEDT(sigmaManualPanel::updateValue));
-	}
+        FloatPreferencePanel sigmaManualPanel = new FloatPreferencePanel(styleManager, I18n.get("settings.standard_deviation_manual"), preferences.sigmaManual);
+        column1.add(sigmaManualPanel);
 
-	private void startCalibrating(boolean isManualCalibrator) {
-		CalibrationDialog d = new CalibrationDialog(styleManager, preferences, calibratorFactory, actionExecutor, owner, isManualCalibrator);
-		d.setLocation(owner.getX() - 140, owner.getY() + 30);
-		styleManager.init();
-		SwingUtilities.invokeLater(() -> d.setVisible(true));
-	}
+        JButton manualCalibrateButton = new FlatButton(styleManager, I18n.get("settings.calibrate_standard_deviation_manual")) {
+            @Override
+            public int getTextSize(SizePreference p) {
+                return p.TEXT_SIZE_SMALL;
+            }
+        };
+        manualCalibrateButton.addActionListener(p -> startCalibrating(true));
+        manualCalibrateButton.setAlignmentX(0.5f);
+        column1.add(manualCalibrateButton);
 
-	private void setAltSigmaEnabled(boolean b) {
-		sigmaAlt.setEnabled(b);
-		sigmaAltHotkey.setEnabled(b);
-	}
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.enable_standard_deviation_toggle"), preferences.useAltStd));
 
+        sigmaAlt = new FloatPreferencePanel(styleManager, I18n.get("settings.alt_standard_deviation"), preferences.sigmaAlt);
+        sigmaAlt.setEnabled(preferences.useAltStd.get());
+        column1.add(sigmaAlt);
+
+        if (KeyboardListener.registered) {
+            sigmaAltHotkey = new HotkeyPanel(styleManager, I18n.get("settings.alt_std_on_last_angle"), preferences.hotkeyAltStd);
+            sigmaAltHotkey.setEnabled(preferences.useAltStd.get());
+            column1.add(sigmaAltHotkey);
+        }
+		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.auto_lock"), preferences.autoLock)); 
+        column2.add(new DoublePreferencePanel(styleManager, I18n.get("settings.crosshair_correction"), preferences.crosshairCorrection));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.show_angle_errors"), preferences.showAngleErrors));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.color_negative_coords"), preferences.colorCodeNegativeCoords));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.use_advanced_stronghold_statistics"), preferences.useAdvStatistics));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.use_alternative_clipboard_reader"), preferences.altClipboardReader));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.enable_api"), preferences.enableHttpServer)
+                .withWikiButton(new WikiButton(styleManager, "https://github.com/Ninjabrain1/Ninjabrain-Bot/wiki/API")));
+        column2.add(new CheckboxPanel(styleManager, I18n.get("settings.save_state"), preferences.saveState));
+
+        disposeHandler.add(preferences.useAltStd.whenModified().subscribeEDT(this::setAltSigmaEnabled));
+        disposeHandler.add(preferences.sigma.whenModified().subscribeEDT(sigmaPanel::updateValue));
+        disposeHandler.add(preferences.sigmaManual.whenModified().subscribeEDT(sigmaManualPanel::updateValue));
+
+    }
+
+    private void startCalibrating(boolean isManualCalibrator) {
+        CalibrationDialog d = new CalibrationDialog(styleManager, preferences, calibratorFactory, actionExecutor, owner, isManualCalibrator);
+        d.setLocation(owner.getX() - 140, owner.getY() + 30);
+        styleManager.init();
+        SwingUtilities.invokeLater(() -> d.setVisible(true));
+    }
+
+    private void setAltSigmaEnabled(boolean b) {
+        sigmaAlt.setEnabled(b);
+        if (sigmaAltHotkey != null)
+            sigmaAltHotkey.setEnabled(b);
+    }
 }
--- src/main/java/ninjabrainbot/gui/options/sections/BasicOptionsPanel.java
+++ src/main/java/ninjabrainbot/gui/options/sections/BasicOptionsPanel.java
@@ -5,42 +5,62 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import ninjabrainbot.event.DisposeHandler;
+import ninjabrainbot.event.IDisposable;
+
 import ninjabrainbot.gui.components.layout.StackPanel;
 import ninjabrainbot.gui.components.preferences.CheckboxPanel;
 import ninjabrainbot.gui.components.preferences.RadioButtonPanel;
+import ninjabrainbot.gui.components.preferences.FloatPreferencePanel;
 import ninjabrainbot.gui.frames.OptionsFrame;
 import ninjabrainbot.gui.style.StyleManager;
 import ninjabrainbot.io.mcinstance.IActiveInstanceProvider;
 import ninjabrainbot.io.preferences.NinjabrainBotPreferences;
 import ninjabrainbot.util.I18n;
 
-public class BasicOptionsPanel extends JPanel {
+public class BasicOptionsPanel extends JPanel implements IDisposable {
 
-	public BasicOptionsPanel(StyleManager styleManager, NinjabrainBotPreferences preferences, IActiveInstanceProvider activeInstanceProvider) {
-		setOpaque(false);
-		setLayout(new GridLayout(1, 2, 2 * OptionsFrame.PADDING, 0));
-		setBorder(new EmptyBorder(2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING));
-		JPanel column1 = new StackPanel(3 * OptionsFrame.PADDING);
-		JPanel column2 = new StackPanel(2 * OptionsFrame.PADDING);
-		column1.setOpaque(false);
-		column2.setOpaque(false);
-		add(column1);
-		add(column2);
+    private final DisposeHandler disposeHandler = new DisposeHandler();
 
-		// Column 1
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.show_nether_coordinates"), preferences.showNetherCoords));
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.auto_reset"), preferences.autoReset));
-		if (activeInstanceProvider.supportsReadingActiveMinecraftWorld())
-			column1.add(new CheckboxPanel(styleManager, I18n.get("settings.auto_reset_when_world_is_reset"), preferences.autoResetWhenChangingInstance));
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.always_on_top"), preferences.alwaysOnTop));
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.translucent_window"), preferences.translucent));
-		column1.add(new CheckboxPanel(styleManager, I18n.get("settings.notify_when_a_new_version_is_available"), preferences.checkForUpdates));
+    public BasicOptionsPanel(StyleManager styleManager, NinjabrainBotPreferences preferences, IActiveInstanceProvider activeInstanceProvider) {
+        setOpaque(false);
+        setLayout(new GridLayout(1, 2, 2 * OptionsFrame.PADDING, 0));
+        setBorder(new EmptyBorder(2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING, 2 * OptionsFrame.PADDING));
 
-		// Column 2
-		column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.display_stronghold_location_using"), preferences.strongholdDisplayType));
-		column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.view_type"), preferences.view));
-		column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.window_size"), preferences.size));
-		column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.mc_version"), preferences.mcVersion));
-	}
+        JPanel column1 = new StackPanel(3 * OptionsFrame.PADDING);
+        JPanel column2 = new StackPanel(2 * OptionsFrame.PADDING);
+        column1.setOpaque(false);
+        column2.setOpaque(false);
+        add(column1);
+        add(column2);
 
+        // Column 1
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.show_nether_coordinates"), preferences.showNetherCoords));
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.auto_reset"), preferences.autoReset));
+        // Auto-reset delay input box
+        FloatPreferencePanel autoResetDelayPanel = new FloatPreferencePanel(styleManager, I18n.get("settings.reset_delay"), preferences.AutoResetDelay);
+        autoResetDelayPanel.setEnabled(preferences.autoReset.get());
+        column1.add(autoResetDelayPanel);
+
+        disposeHandler.add(preferences.autoReset.whenModified().subscribeEDT(p -> {
+            autoResetDelayPanel.setEnabled(preferences.autoReset.get());
+        }));
+        if (activeInstanceProvider.supportsReadingActiveMinecraftWorld())
+            column1.add(new CheckboxPanel(styleManager, I18n.get("settings.auto_reset_when_world_is_reset"), preferences.autoResetWhenChangingInstance));
+
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.always_on_top"), preferences.alwaysOnTop));
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.translucent_window"), preferences.translucent));
+        column1.add(new CheckboxPanel(styleManager, I18n.get("settings.notify_when_a_new_version_is_available"), preferences.checkForUpdates));
+
+        // Column 2
+        column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.display_stronghold_location_using"), preferences.strongholdDisplayType));
+        column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.view_type"), preferences.view));
+        column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.window_size"), preferences.size));
+        column2.add(new RadioButtonPanel(styleManager, I18n.get("settings.mc_version"), preferences.mcVersion));
+    }
+
+    @Override
+    public void dispose() {
+        disposeHandler.dispose();
+    }
 }
