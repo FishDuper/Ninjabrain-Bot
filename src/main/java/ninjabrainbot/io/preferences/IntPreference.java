9056c2440b7ad56d6dfd66c961a1cc2252eee7b9
FishDuper <gsgsgs03gs@gmail.com>
2026-09-03 23:18:14 -0500

    Update IntPreference.java
    Override for actual IntPreferences such as those used with timers.

--- src/main/java/ninjabrainbot/io/preferences/IntPreference.java
+++ src/main/java/ninjabrainbot/io/preferences/IntPreference.java
@@ -8,15 +8,40 @@ public class IntPreference extends Modifiable<Integer> {
 
 	final String key;
 	int value;
+	int min;
+	int max;
 
-	public IntPreference(String key, int defaultValue, IPreferenceSource pref) {
+	
+	
+	public IntPreference(String key, int defaultValue, int minValue, int maxValue, IPreferenceSource pref) { // override for actual IntPreferences
 		this.pref = pref;
 		this.key = key;
 		value = pref.getInt(key, defaultValue);
+		this.min = minValue;
+		this.max = maxValue;
+		if (value > max)
+			value = max;
+		if (value < min)
+			value = min;
 	}
 
+
+	public IntPreference(String key, int defaultValue, IPreferenceSource pref) { // This is important to keep for hotkeys, themes, et cetera.
+		this.pref = pref;
+		this.key = key;
+		value = pref.getInt(key, defaultValue);	
+	}
+	
 	public int get() {
 		return value;
+	}
+
+	public double max() {
+		return max;
+	}
+
+	public double min() {
+		return min;
 	}
 
 	public void set(int value) {
@@ -24,5 +49,5 @@ public class IntPreference extends Modifiable<Integer> {
 		pref.putInt(key, value);
 		notifySubscribers(value);
 	}
-
+	
 }
